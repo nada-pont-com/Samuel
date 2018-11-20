4572617488b7882c1719aa0e3881005a1ce283d5
Your Name <you@example.com>
2018-11-20 16:53:36 -0200

    a

--- WebContent/script/ednGame.js
+++ WebContent/script/ednGame.js
@@ -10,6 +10,9 @@ class EndGame extends Phaser.Scene{
         this.add.image(0,0,"Preto").setOrigin(0,0);
         let texto = this.add.text(350,144,"Fim de Jogo",{fill:"#fff",fontSize:"40px",fontFamily:"Arial"});
         texto.setOriginDisplay(texto.width/2,texto.height/2);
+        start.on('pointerdown', function(){
+            this.scene.start('MenuFases');
+        }, this);
     }
 }
 export default EndGame;
